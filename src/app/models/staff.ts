3384461f179951f4d9cbf55f9bc2c060cf7a4836
shadabdsw <shadab1996@gmail.com>
2021-08-13 10:16:44 +0530

    login auth

---
+++ src/app/models/staff.ts
@@ -0,0 +1,5 @@
+export interface Staff {
+  uid?: string;
+  email: string;
+  password: string;
+}
--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -8,28 +9,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./temp.component.css'],
 })
 export class TempComponent implements OnInit {
-  @Input() public parentData: any;
   @Output() public loginEvent1 = new EventEmitter();
 
   username = new FormControl('', [Validators.required]);
   password = new FormControl('', [Validators.required]);
 
+  // username1 = (document.getElementById('username1') as HTMLInputElement).value;
+  // password1 = (document.getElementById('password1') as HTMLInputElement).value;
+
   hide = true;
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private snackBar: MatSnackBar, private auth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
-    if (
-      (document.getElementById('username') as HTMLInputElement).value ==
-        'admin' &&
-      (document.getElementById('password') as HTMLInputElement).value == 'admin'
-    ) {
-      this.loginEvent1.emit(true);
-      this.snackBar.open('Login Successful!', '', { duration: 2000 });
-    } else {
-      this.snackBar.open('Incorrect Credentials!', '', { duration: 2000 });
-    }
+    // if (
+    //   (document.getElementById('username') as HTMLInputElement).value ==
+    //     'admin' &&
+    //   (document.getElementById('password') as HTMLInputElement).value == 'admin'
+    // ) {
+    //   this.loginEvent1.emit(true);
+    //   this.snackBar.open('Login Successful!', '', { duration: 2000 });
+    // } else {
+    //   this.snackBar.open('Incorrect Credentials!', '', { duration: 2000 });
+    // }
+
+    this.auth
+      .signInWithEmailAndPassword(this.username.value, this.password.value)
+      .then((res) => {
+        if (res) {
+          this.loginEvent1.emit(true);
+          this.snackBar.open('Login Successful!', '', { duration: 2000 });
+        } else {
+          this.snackBar.open('Incorrect Credentials!', '', { duration: 2000 });
+        }
+      });
   }
 }
